981334971aa3dc5e228d0edd148e41cd09c3e7fa
Angga Puja Restu Prakasa <kierska19@gmail.com>
2023-02-08 12:55:26 +0700

    Fix Firebase Bug
    Took 4 hours 39 minutes

--- app/src/main/java/com/gubi/iotapps/MainActivity.java
+++ app/src/main/java/com/gubi/iotapps/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Menyembunyikan Action Bar
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
